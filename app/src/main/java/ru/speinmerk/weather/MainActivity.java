b5b1a2cc9e0aea3bea6c0f490b0bd95b58ce8984
Speinmerk <Speinmerk@gmail.com>
2018-05-21 18:17:29 +0400

    Lesson 3

--- app/src/main/java/ru/speinmerk/weather/MainActivity.java
+++ app/src/main/java/ru/speinmerk/weather/MainActivity.java
@@ -1,9 +1,14 @@
 package ru.speinmerk.weather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import ru.speinmerk.weather.models.City;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,40 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ShowMessage(getString(R.string.on_create));
+
+        EditText etCity = findViewById(R.id.et_city);
+        Button btnOk = findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(v -> {
+            String nameCity = etCity.getText().toString();
+            if (nameCity.isEmpty()) {
+                Toast.makeText(getApplicationContext(), getString(R.string.enter_city), Toast.LENGTH_SHORT).show();
+            } else {
+                startActivityWeatherForCity(new City(nameCity,25, 50, 5, 750));
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ShowMessage(getString(R.string.on_start));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ShowMessage(getString(R.string.on_resume));
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        ShowMessage(getString(R.string.on_pause));
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        ShowMessage(getString(R.string.on_stop));
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ShowMessage(getString(R.string.on_destroy));
+    private void startActivityWeatherForCity(City city) {
+        Intent intent = new Intent(this, WeatherForCity.class);
+        intent.putExtra(City.KEY_PARCELABLE, city);
+        startActivity(intent);
     }
 
     private void ShowMessage(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         Log.i(getClass().getSimpleName(), message);
     }
 }
---
+++ app/src/main/java/ru/speinmerk/weather/WeatherForCity.java
@@ -0,0 +1,31 @@
+package ru.speinmerk.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ru.speinmerk.weather.models.City;
+
+public class WeatherForCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_for_city);
+
+        City city = getIntent().getParcelableExtra(City.KEY_PARCELABLE);
+
+        TextView tvCity = findViewById(R.id.tv_city);
+        TextView tvTemperature = findViewById(R.id.tv_temperature);
+        TextView tvHumidity = findViewById(R.id.tv_humidity);
+        TextView tvWindSpeed = findViewById(R.id.tv_wind_speed);
+        TextView tvPressure = findViewById(R.id.tv_pressure);
+
+        tvCity.setText(String.format("Город %s", city.name));
+        tvTemperature.setText(String.format("Температура: %s", city.temperature));
+        tvHumidity.setText(String.format("Влажность: %s", city.humidity));
+        tvWindSpeed.setText(String.format("Скорость ветра: %s м/с", city.windSpeed));
+        tvPressure.setText(String.format("Давление: %s мм рт.ст.", city.pressure));
+    }
+}
